0d1ba7d7c6f5c20d576e41bb07053ee6033b6782
Mirko Di Cristina <mirko7726@gmail.com>
2022-08-04 12:05:15 +0200

    Javadoc creation in the interface

--- src/SortableSequence.java
+++ src/SortableSequence.java
@@ -1,6 +1,10 @@
+/**
+ * @author Mirko Di Cristina
+ * @param <T> Generic parameter.
+ * Implementation of methods for sorting.
+ */
 public interface SortableSequence<T> {
 
    void sort();
-     
-    boolean isSorted();
+   boolean isSorted();
 }
