72491a6b2b1e308540e5fb5e9bfcf0b8a9173ff1
Sow <Sow@sow.sow>
2022-08-04 12:26:03 +0200

    Game class implementation and insert javadoc

--- src/Game.java
+++ src/Game.java
@@ -1,43 +1,46 @@
 /**
  * @author Soheil
+ * I create a class Game that implement Comaparable with some method
  */
 public class Game implements Comparable<Game> {
 
     int enemies;
     double score;
 
-
-public Game(int enemies, double score) {
-this.enemies = enemies;
-this.score = score;
-}
+    /**
+     * A Constructor of Games
+     * @param enemies number of enemies
+     * @param score   games  score
+     */
+    public Game(int enemies, double score) {
+        this.enemies = enemies;
+        this.score = score;
+    }
 
     /**
      * each enemy killed have score
-     * @return score of enenmy killed
+     * @return score of enemy killed
      */
     public double getFinalScore() {
-return enemies*score;
+        return enemies * score;
+    }
 
-}
-
- @Override
- public String toString() {
- return "Game{" +
- "enemies=" + enemies +
- ", score=" + score +
- '}';
- }
+    @Override
+    public String toString() {
+        return "Game{" +
+                "enemies=" + enemies +
+                ", score=" + score +
+                '}';
+    }
 
     /**
-     *
      * @param o to compare enemy score
-     * @return who have more score
+     * @return The method the comparison of player final score
      */
- @Override
- public int compareTo(Game o) {
- return Double.compare(this.getFinalScore(), o.getFinalScore());
- }
- }
+    @Override
+    public int compareTo(Game o) {
+        return Double.compare(this.getFinalScore(), o.getFinalScore());
+    }
+}
 
 
